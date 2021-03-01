02a363f00da7354f66e00e5adda5fe8df01206bf
Saulo Coelho <coelho.saulo@gmail.com>
2021-03-01 15:23:24 -0300

    atualizando componentes

---
+++ src/pages/Report/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Picker, StyleSheet, Button} from 'react-native';
+
+import BalanceLabel from '../../components/BalancePanel';
+import EntrySummary from '../../components/EntrySummary';
+import EntryList from '../../components/EntryList';
+
+const Report = () => {
+  const currentBalance = 2064.35;
+
+  const entriesGrouped = [
+    {key: '1', description: 'Alimentação:', amount: 201},
+    {key: '2', description: 'Combustível:', amount: 12},
+    {key: '3', description: 'Aluguel:', amount: 120},
+    {key: '4', description: 'Lazer:', amount: 250},
+    {key: '5', description: 'Outros:', amount: 1200},
+  ];
+
+  const entries = [
+    {key: '1', description: 'Padaria Asa Branca:', amount: 10},
+    {key: '2', description: 'Supermercado Isadora:', amount: 190},
+    {key: '3', description: 'Posto Ipiranga: $190', amount: 290},
+  ];
+
+  return (
+    <View>
+      <BalanceLabel currentBalance={currentBalance} />
+      <View>
+        <Picker>
+          <Picker.Item label="Todas Categorias" />
+        </Picker>
+        <Picker>
+          <Picker.Item label="Ultimos sete dias" />
+        </Picker>
+      </View>
+      <EntrySummary entriesGrouped={entriesGrouped} />
+      <EntryList entries={entries} />
+      <View>
+        <Button title="Salvar " />
+        <Button title="Fechar" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+  },
+});
+
+export default Report;
