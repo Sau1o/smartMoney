154a89dbcd437666888b9581364ff922021383d2
Saulo Coelho <coelho.saulo@gmail.com>
2021-04-13 10:37:24 -0300

    change package name

--- android/app/src/main/java/br/com/saulocoelho/smartmoney_curso/MainApplication.java
+++ android/app/src/main/java/br/com/saulocoelho/smartmoney_curso/MainApplication.java
@@ -1,4 +1,4 @@
-package com.smartmoney;
+package br.com.saulocoelho.smartmoney_curso;
 
 import android.app.Application;
 import android.util.Log;
--- ios/MyMoneyTests/MyMoneyTests.m
+++ ios/MyMoneyTests/MyMoneyTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface smartMoneyTests : XCTestCase
+@interface MyMoneyTests : XCTestCase
 
 @end
 
-@implementation smartMoneyTests
+@implementation MyMoneyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
